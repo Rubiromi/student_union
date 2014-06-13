a076beb0d0bf48964157eb8c09c3838057224d8e
Rubiromi <hiromi.deyoung@gmail.com>
2014-06-13 09:11:00 -0600

    delete is working. edit is not working.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,48 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update_attributes(student_params)
+      redirect_to @student
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def destroy
+    Student.find(params[:id]).destroy
+    redirect_to students_path
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :course)
+  end
+end
